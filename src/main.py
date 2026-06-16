96362c2de4b7259621bf66ef99af665ee0fa482e
Yanick Schriefers <y.schriefers@gmail.com>
2026-06-16 13:02:56 +0200

    rewritten search and selection: search accepts pretty much anything and gives u the uuid, which you then use to read data, modify, delete etc

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import json
 import logging
-import uuid
-from pathlib import Path
+import uuid as uuid_module
 from typing import TypedDict
 
 import pubchempy
@@ -12,7 +11,6 @@ from config import db_path
 
 
 class Molecule(TypedDict):
-    uuid: str
     inchi: str
     inchikey: str
     smiles: str
@@ -27,77 +25,67 @@ class Database:
         self.molecules_path.mkdir(parents=True, exist_ok=True)
 
         self.by_uuid: dict[str, Molecule] = {}
-        self.by_inchi: dict[str, Molecule] = {}
-        self.by_inchikey: dict[str, Molecule] = {}
-        self.by_smiles: dict[str, Molecule] = {}
-        self.by_cas: dict[str, Molecule] = {}
-        self.by_name: dict[str, Molecule] = {}
+        self.by_inchi: dict[str, str] = {}
+        self.by_inchikey: dict[str, str] = {}
+        self.by_smiles: dict[str, str] = {}
+        self.by_cas: dict[str, str] = {}
+        self.by_name: dict[str, str] = {}
 
         self.read_db_metadata()
 
     def search_in_db(
         self,
         *,
-        uuid: str | None = None,
         inchi: str | None = None,
         inchikey: str | None = None,
         smiles: str | None = None,
-        cas: str | None,
-        compound_name: str | None,
-    ) -> Molecule | None:
-        result: Molecule | None = None
-        if uuid is not None:
-            result = self.by_uuid.get(uuid, None)
-        elif inchi is not None:
-            result = self.by_inchi.get(inchi, None)
+        cas: str | None = None,
+        compound_name: str | None = None,
+        molecule: Molecule | None = None,
+    ) -> str | None:
+        if molecule is not None:
+            inchi = molecule["inchi"]
+        if inchi is not None:
+            if not inchi.startswith("InChI="):  # the "InChI=" part is actually part of the inchi, but thats not really intuitive so this adds it
+                inchi = f"InChI={inchi}"
+            return self.by_inchi.get(inchi, None)
         elif inchikey is not None:
-            result = self.by_inchikey(inchikey, None)
+            return self.by_inchikey(inchikey, None)
         elif smiles is not None:
-            result = self.by_smiles.get(smiles, None)
+            return self.by_smiles.get(smiles, None)
         elif cas is not None:
-            result = self.by_cas.get(cas, None)
+            return self.by_cas.get(cas, None)
         elif compound_name is not None:
-            result = self.by_name.get(compound_name, None)
-        else:
-            raise ValueError("No search query given, can't search.")
+            return self.by_name.get(compound_name, None)
 
-        return result
-
-    def is_this_in_db(self, molecule: Molecule) -> bool:
-        if self.by_uuid.get(molecule["uuid"], None):
+    def is_this_in_db(self, *, uuid: str | None = None, molecule: Molecule | None = None) -> bool:
+        if self.by_uuid.get(uuid, None):
             return True
         if self.by_inchikey.get(molecule["inchikey"], None):
             return True
         return False
 
     def create_entry(self, molecule: Molecule) -> bool:
-        if self.is_this_in_db(molecule):
+        if self.is_this_in_db(molecule=molecule):
             logging.error("Molecule could not be added to database, because such a molecule is already in there.")
             return False
 
-        self.write_metadata_file(molecule=molecule)
+        molecule_uuid = str(uuid_module.uuid4())
+
+        self.write_metadata_file(uuid=molecule_uuid, molecule=molecule)
 
     def delete_entry(
         self,
-        *,
         uuid: str | None = None,
-        inchi: str | None = None,
-        inchikey: str | None = None,
-        smiles: str | None = None,
-        cas: str | None = None,
-        compound_name: str | None = None,
     ) -> bool:
-        result = self.search_in_db(uuid=uuid, inchi=inchi, inchikey=inchikey, smiles=smiles, cas=cas, compound_name=compound_name)
-        if result is None:
-            raise ValueError("The given molecule could not be found in the database, so it can't be deleted.")
 
-        print(f"Found in database: {result}")
+        print(f"Found in database: {self.by_uuid[uuid]}")
         confirmation = str(input("Are you sure to delete that? y(es) / n(o) :"))
         if any(confirmation == x for x in ("yes", " yes", "yes ", " yes ", "y", " y", "y ", " y ")):
             print("Deleting molecule from database...")
-            deletion_path = self.molecules_path / result["uuid"]
+            deletion_path = self.molecules_path / uuid
             if not deletion_path.is_dir():
-                raise FileNotFoundError("Could not delete molecule %s from db, because the path %s could not be found.", result, deletion_path)
+                raise FileNotFoundError("Could not delete molecule %s from db, because the path %s could not be found.", uuid, deletion_path)
             send2trash(deletion_path)
             print("Deleting molecule from database... Done!")
             return True
@@ -107,21 +95,13 @@ class Database:
 
     def add_entry_names(
         self,
-        *,
         uuid: str | None = None,
-        inchi: str | None = None,
-        inchikey: str | None = None,
-        smiles: str | None = None,
-        cas: str | None = None,
-        compound_name: str | None = None,
     ) -> bool:
-        result = self.search_in_db(uuid=uuid, inchi=inchi, inchikey=inchikey, smiles=smiles, cas=cas, compound_name=compound_name)
-        if result is None:
-            raise ValueError("The given molecule could not be found in the database, so it can't be deleted.")
-
+        result = self.by_uuid[uuid]
         print(f"Current names: {result["names"]}")
 
-        add = input("Please enter additional name:")
+        msg = "Please enter additional name (press ENTER without typing anything to save your changes):"
+        add = input(msg)
         while add:
             while add.startswith(" "):
                 add = add[1:]
@@ -133,26 +113,17 @@ class Database:
             else:
                 result["names"] = result["names"] + [add]
 
-            add = input("Please enter additional name:")
+            add = input(msg)
 
-        self.write_metadata_file(molecule=result)
+        self.write_metadata_file(uuid=uuid, molecule=result)
 
         return True
 
     def modify_entry_cas(
         self,
-        *,
         uuid: str | None = None,
-        inchi: str | None = None,
-        inchikey: str | None = None,
-        smiles: str | None = None,
-        cas: str | None = None,
-        compound_name: str | None = None,
     ) -> None:
-        result = self.search_in_db(uuid=uuid, inchi=inchi, inchikey=inchikey, smiles=smiles, cas=cas, compound_name=compound_name)
-        if result is None:
-            raise ValueError("The given molecule could not be found in the database, so it can't be deleted.")
-
+        result = self.by_uuid[uuid]
         print(f"Selected molecule: {result}")
 
         add = input("Please enter CAS:")
@@ -162,51 +133,48 @@ class Database:
             add = add[:-1]
         result["cas"] = add
 
-        self.write_metadata_file(molecule=result)
+        self.write_metadata_file(uuid=uuid, molecule=result)
 
         return True
 
-    def read_metadata_file(self, file_path: Path) -> Molecule:
+    def read_metadata_file(self, uuid: str) -> Molecule:
+        file_path = self.molecules_path / uuid / "metadata.json"
         if not file_path.exists() and file_path.is_file():
             raise ValueError("Path at %s does not point at file, cannot read.", str(file_path))
 
         with open(file_path, encoding="utf-8") as file:
             data = json.load(file)
 
-        uuid = data.get("uuid", None)
         inchi = data.get("inchi", None)
         inchikey = data.get("inchikey", None)
         smiles = data.get("smiles", None)
         cas = data.get("cas", None)
         names = data.get("names", None)
 
-        if any(val is None for val in (uuid, inchi, inchikey, smiles)):
+        if any(val is None for val in (inchi, inchikey, smiles)):
             raise ValueError("Molecule metadata file at path %s is invalid, arguments missing.", file_path)
 
-        return Molecule(uuid=uuid, inchi=inchi, inchikey=inchikey, smiles=smiles, cas=cas, names=names)
+        return Molecule(inchi=inchi, inchikey=inchikey, smiles=smiles, cas=cas, names=names)
 
-    def write_metadata_file(self, *, file_path: Path | None = None, molecule: Molecule | None = None) -> None:
-        if file_path is None:
-            if molecule is None:
-                raise ValueError
-            file_path = self.molecules_path / molecule["uuid"] / "metadata.json"
+    def write_metadata_file(self, *, uuid: str, molecule: Molecule) -> None:
+        file_path = self.molecules_path / uuid / "metadata.json"
         file_path.parent.mkdir(parents=True, exist_ok=True)
         with open(file_path, "w", encoding="utf-8") as file:
             file.write(json.dumps(molecule, indent=4, sort_keys=True))
 
     def read_db_metadata(self) -> None:
-        for uuid_molecule in self.molecules_path.iterdir():
-            metadata_file = uuid_molecule / "metadata.json"
-            molecule = self.read_metadata_file(metadata_file)
-            self.by_uuid[molecule["uuid"]] = molecule
-            self.by_inchi[molecule["inchi"]] = molecule
-            self.by_inchikey[molecule["inchikey"]] = molecule
-            self.by_smiles[molecule["smiles"]] = molecule
+        for uuid_molecule_path in self.molecules_path.iterdir():
+            uuid = uuid_molecule_path.stem
+            molecule = self.read_metadata_file(uuid)
+            self.by_uuid[uuid] = molecule
+            self.by_inchi[molecule["inchi"]] = uuid
+            self.by_inchikey[molecule["inchikey"]] = uuid
+            self.by_smiles[molecule["smiles"]] = uuid
             if molecule["cas"]:
-                self.by_cas[molecule["cas"]] = molecule
+                self.by_cas[molecule["cas"]] = uuid
             if molecule["names"]:
                 for name in molecule["names"]:
-                    self.by_name[name] = molecule
+                    self.by_name[name] = uuid
 
 
 def pubchemsearchselection(compounds: list[pubchempy.Compound]) -> pubchempy.Compound:
@@ -236,8 +204,6 @@ def generate_molecule_data(
     cas: str | None = None,
     compound_name: str | None = None,
 ) -> Molecule:
-    molecule_uuid = str(uuid.uuid4())
-
     if smiles:
         molecule_rdkitmol = Chem.MolFromSmiles(smiles, sanitize=True)
     elif inchi:
@@ -263,13 +229,14 @@ def generate_molecule_data(
     names = None
     if compound_name is not None:
         names = [compound_name]
-    return Molecule(uuid=molecule_uuid, inchi=inchi_str, inchikey=inchikey, smiles=canonical_smiles, cas=cas, names=names)
+    return Molecule(inchi=inchi_str, inchikey=inchikey, smiles=canonical_smiles, cas=cas, names=names)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    m = generate_molecule_data(compound_name="toluene")
-    print(m)
     db = Database()
-    db.modify_entry_cas(compound_name="toluene")
-# TODO(TheTimebreaker) next step: add data layer
+    uuid = db.search_in_db(compound_name="toluene")
+    db.modify_entry_cas(uuid)
+    print(db.by_uuid[uuid])
+
+# TODO(TheTimebreaker): next step: add data layer
