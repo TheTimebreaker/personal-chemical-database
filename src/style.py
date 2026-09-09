9639fff0480ae926c4598b407edaf33d79cf4772
Yanick Schriefers <y.schriefers@gmail.com>
2026-09-09 15:08:47 +0200

    added style configure function for fonts

--- src/style.py
+++ src/style.py
@@ -1,4 +1,30 @@
+import tkinter as tk
+import tkinter.font as tkfont
 from tkinter import ttk
+
+
+def style_root(root: tk.Tk) -> None:
+    sans_fonts = (
+        "TkDefaultFont",
+        "TkTextFont",
+        "TkMenuFont",
+        "TkHeadingFont",
+        "TkCaptionFont",
+        "TkSmallCaptionFont",
+        "TkIconFont",
+        "TkTooltipFont",
+    )
+
+    for font_name in sans_fonts:
+        try:
+            tkfont.nametofont(font_name).configure(family="sans-serif")
+        except tk.TclError:
+            pass
+
+    try:
+        tkfont.nametofont("TkFixedFont").configure(family="monospace")
+    except tk.TclError:
+        pass
 
 
 def configure_ttk_style() -> None:
